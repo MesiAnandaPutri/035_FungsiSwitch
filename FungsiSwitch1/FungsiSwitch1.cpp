68af98944b6140d5280477778a1c923f310656a8
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-03-26 17:51:04 +0700

    memasukan input

--- FungsiSwitch1/FungsiSwitch1.cpp
+++ FungsiSwitch1/FungsiSwitch1.cpp
@@ -2,3 +2,15 @@
 //
 
 #include <iostream>
+
+using namespace std;
+
+int bilA, bilB;
+
+void input()
+{
+    cout << "Masukan bilangan pertama = ";
+    cin >> bilA;
+    cout << "Masukan bilangan kedua = ";
+    cin >> bilB;
+}
